208821c9e04e635ad6e664a21873302183c767e4
Andrea Guarise <guarise@localhost.localdomain>
2013-04-19 09:15:04 -0400

    clean for 4_2_0

--- src-hlr-service/regression/tests/hlrServerdIsAlive.sh
+++
@@ -1,7 +0,0 @@
-echo $HLRD_CONF
-eval LOCKFILE=`grep hlr_def_lock $HLRD_CONF | grep "^#" -v | cut -d'"' -f 2`
-[ -e $LOCKFILE ] || exit 11
-PID=`cat $LOCKFILE`
-echo $PID
-echo $LISTENER_NAME
-ps w -p $PID | grep $LISTENER_NAME
